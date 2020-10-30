7df2f907141a6b562fda38cb25c672c64d082a8b
Manogna <manogna.ratnala@outlook.com>
2020-10-30 15:05:09 +0530

    Due to req change.

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -10,7 +10,7 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("hello world");
             Console.ReadLine();
         }
     }
